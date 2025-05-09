5d2a4a11c56982c2a18a98583d5ccb2d6df7026d
albertito1985 <anulo@live.se>
2025-05-09 19:16:28 +0200

    grund avslutad

--- bishBosh/bishBosh.js
+++ bishBosh/bishBosh.js
@@ -1,6 +1,3 @@
-//on load of the page, run the function
-/* bishBosh(); */
-
 document.getElementById("myForm").addEventListener("submit", validationForm);
 
 function bishBosh(bish, bosh, loops) {
@@ -40,13 +37,11 @@ function validationForm(e){
     e.preventDefault();
     let validation = true;
     let elements= e.target.elements
-    console.log(elements.bish.value)
     for(element of elements){
         if(element.value>0){
             element.classList.add("is-valid");
         }else{
             if(element.type != "submit"){
-                console.log(element);
                 validation= false;
                 element.classList.add("is-invalid");
             }
